d127288740ce88fa99bc7c64b23e15ab1f713cba
Sam Humphreys <s-humphreys@hotmail.com>
2016-05-08 17:15:30 +1000

    user CRUD functionality added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,32 @@
 class UsersController < ApplicationController
   before_action :authorise, :only => [:index]
+  before_action :logged_in, :only => [:show, :edit, :update, :destroy]
 
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new user_params
+
+    if params[:user]['image'].present?
+      req = Cloudinary::Uploader.upload(params[:user]['image'])
+      @user = User.new(user_params)
+      @user.udpate :image => req['url']
+    else
+      @user = User.new(user_params)
+      @user.update :image => 'http://www.priorlakeassociation.org/wp-content/uploads/2011/06/blank-profile.png'
+    end
 
     if @user.save
       session[:user_id] = @user.id
-      redirect_to edit_user_path(@user.id)
+      redirect_to root_path
     else
       render :new
     end
   end
 
   def index
-    @users = User.all
+    @users = User.all.sort_by {|user| user.id}
   end
 
   def edit
@@ -36,6 +45,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find params[:id]
+    if @user.last.present?
+      @initial = @user.last[0]
+    end
   end
 
   def destroy
@@ -51,6 +63,10 @@ class UsersController < ApplicationController
 
   def authorise
     redirect_to root_path unless (@current_user.present? && @current_user.admin?)
+  end
+
+  def logged_in
+    redirect_to root_path unless @current_user.present?
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+
+u1 = User.create :first => 'Sam', :last => 'Humphreys', :password => 'chicken', :email => 'sam@ga.co', :admin => true
+u2 = User.create :first => 'Bill', :last => 'Murray', :password => 'chicken', :email => 'bill@ga.co', :image => 'fillmurray.com/300/300'
+u3 = User.create :first => 'Charlie', :last => 'Sheen', :password => 'chicken', :email => 'charlie@ga.co'
+u4 = User.create :first => 'Nick', :last => 'Cage', :password => 'chicken', :email => 'nick@ga.co', :image => 'placecage.com/c/300/300'
