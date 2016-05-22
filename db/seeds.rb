037ef3be267e6924ec02ed83f5beaeae648429f1
Sam Humphreys <s-humphreys@hotmail.com>
2016-05-22 23:21:55 +1000

    added minimal styling to user form

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 User.destroy_all
 
-u1 = User.create :first => 'Sam', :last => 'Humphreys', :password => 'chicken', :email => 'sam@ga.co', :admin => true, :image => 'http://res.cloudinary.com/iamasamwich/image/upload/v1462686798/qf42dxcmfwumiqrkiqqe.jpg'
+u1 = User.create :first => 'Sam', :last => 'Humphreys', :password => 'Adminchicken', :email => 'sam@ga.co', :admin => true, :image => 'http://res.cloudinary.com/iamasamwich/image/upload/v1462686798/qf42dxcmfwumiqrkiqqe.jpg'
 u2 = User.create :first => 'Bill', :last => 'Murray', :password => 'chicken', :email => 'bill@ga.co', :image => 'http://res.cloudinary.com/iamasamwich/image/upload/v1462689165/m1jriyo8d2cc7t4lcyrs.jpg'
 u3 = User.create :first => 'Charlie', :last => 'Sheen', :password => 'chicken', :email => 'charlie@ga.co', :image => 'http://res.cloudinary.com/iamasamwich/image/upload/v1461291658/rhyoiyxjcgtfvz87wn7b.gif'
 u4 = User.create :first => 'Nick', :last => 'Cage', :password => 'chicken', :email => 'nick@ga.co', :image => 'http://res.cloudinary.com/iamasamwich/image/upload/v1461509967/omrqt5likrdvf5jsi4f2.jpg'
